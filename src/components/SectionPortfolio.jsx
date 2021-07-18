b5bc556f753ddc7243ac6b42b853dd29f303e896
Anyelo Cruz <anyelo.cruz.c@gmail.com>
2021-07-17 23:05:34 -0400

    solucionado problemas con media queries

--- src/components/SectionPortfolio.jsx
+++ src/components/SectionPortfolio.jsx
@@ -3,7 +3,7 @@ import '../styles/components/SectionPortfolio.scss'
 
 const SectionPortfolio = () => (
   <section className="section__portfolio">
-    <p>Actualmente éste blog es mi único proyecto personal. creado en un inicio con HTML y Sass, luego migrado a ReactJS con Webpack, para retarme a mí mismo de lograr terminar mi blog.
+    <p>Actualmente éste blog es mi único proyecto personal. creado en un inicio con HTML y Sass, luego migrado a ReactJS en configuración con Webpack, para retarme a terminar mi blog.
     Pronto haré nuevas actualizaciones al blog, como agregar un formulario de contacto, además de crear nuevo contenido para el blog.</p>
     <p>Tengo dos ideas de proyectos, crear una e-commerce, con HTML y JavaScript, para practicar y mejorar la manipulación del DOM y crear web components. Otra idea que tengo es crear un portal de noticias utilizando Materialize, para practicar con este framework de CSS.</p>
     <p>A medida que obtenga más conocimientos y habilidades las iré compartiendo aquí.</p>
